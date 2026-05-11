2dc77d882471027cdb8a9ed16218c9fe13a8788b
Yousef_waleed <Dev.yousef.waleed@gmail.com>
2026-05-11 17:06:08 +0300

    add: api routes

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\AuthController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Notes
+    Route::resource('notes', NoteController::class)->only(['index','store','update','delete']);
+    //archive
+    Route::patch('/notes/{id}/toggleArchive', [NoteController::class, 'toggleArchive']);
+
+    //tags
+    Route::get('/tags', function (Request $request) {
+        return response()->json($request->user()->tags()->select('id', 'name')->get());
+    });
+
+    //auth
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
