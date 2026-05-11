83c8618c5440d6b5eca8739df05a73f547272c79
Yousef_waleed <Dev.yousef.waleed@gmail.com>
2026-05-11 17:02:35 +0300

    add:migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_image')->nullable();
+            $table->boolean('is_active')->default(false);
+            $table->boolean('is_verfied')->default(false);            
             $table->rememberToken();
             $table->timestamps();
         });
